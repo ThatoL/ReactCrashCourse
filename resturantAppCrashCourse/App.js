d9e89fe37bf4e69711ba2f3808db02a80de564ed
ThatoL <t.lebethe93@gmail.com>
2022-06-16 20:10:30 +0200

    applied flatlist to category items card, added array of objects to carry card data

--- resturantAppCrashCourse/App.js
+++ resturantAppCrashCourse/App.js
@@ -1,28 +1,55 @@
-import {View , Text, StyleSheet} from 'react-native';
+import {View , StyleSheet, FlatList} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import Header from './src/components/Header';
 import Search from './src/components/Search';
 import CategoryItem from './src/components/CategoryItem';
 
 export default function App(){
+  const commonCategories = [
+    {
+      name:'cake',
+      imageUrl:require('./src/assets/images/cake-slice.png')
+    },
+    {
+      name:'drinks',
+      imageUrl:require('./src/assets/images/cocktail.png')
+    },
+    {
+      name:'burger',
+      imageUrl:require('./src/assets/images/hamburger.png')
+    },
+    {
+      name:'meat',
+      imageUrl:require('./src/assets/images/meat.png')
+    },
+    {
+      name:'snacks',
+      imageUrl:require('./src/assets/images/nachos.png')
+    },
+    {
+      name:'pizza',
+      imageUrl:require('./src/assets/images/pizza.png')
+    }
+  ]
+
   return (
     <View style={styles.container}>
       <Header/>
       <Search />
-      <CategoryItem
-        name='burger'
-        imageUrl = {require('./src/assets/images/hamburger.png')}
+      <FlatList
+        keyExtractor={(category) => category.name} 
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data= {commonCategories}
+        renderItem={({item, index})=> {
+          return (<CategoryItem 
+            name={item.name}
+            imageUrl={item.imageUrl}
+            index={index}
+          />)
+        }}
       />
 
-      <CategoryItem
-        name='pizza'
-        imageUrl = {require('./src/assets/images/pizza.png')}
-      />
-
-      <CategoryItem
-        name='steak'
-        imageUrl = {require('./src/assets/images/meat.png')}
-      />
     <StatusBar />
     </View>
 
