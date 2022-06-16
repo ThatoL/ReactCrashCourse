db35a42c08349d0adc7b55bdf53fb6f2c60ad74b
ThatoL <t.lebethe93@gmail.com>
2022-06-16 19:20:35 +0200

    category item card complete

---
+++ resturantAppCrashCourse/src/components/CategoryItem.js
@@ -0,0 +1,48 @@
+import { View, Text , StyleSheet, Image} from 'react-native'
+import React from 'react'
+import {elevation} from '../common/style'
+
+export default function CategoryItem() {
+  return (
+    <View style={[styles.container, styles.elevation]}>
+        <View style={styles.imageContainer}>
+            <Image 
+                source={require('../assets/images/pizza.png')}
+                style={styles.image}
+            />
+        </View>
+      <Text style={styles.text}>Burger</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    elevation,
+    container: {
+        width: 70,
+        height: 100,
+        borderRadius: 50,
+        marginVertical: 15,
+        backgroundColor: 'white',
+        marginHorizontal: 25,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    image: {
+        width: 35,
+        height: 35,
+    },
+    imageContainer: {
+        width: 50,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        marginBottom: 5,
+        backgroundColor: 'white',
+    },
+    text : {
+        fontWeight: 'bold',
+    }
+})
