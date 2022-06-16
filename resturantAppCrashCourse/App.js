2daa8d46793b4702a417e1b16bfc3688f1268bf7
ThatoL <t.lebethe93@gmail.com>
2022-06-16 19:18:53 +0200

    rendered single category item

--- resturantAppCrashCourse/App.js
+++ resturantAppCrashCourse/App.js
@@ -2,12 +2,14 @@ import {View , Text, StyleSheet} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import Header from './src/components/Header';
 import Search from './src/components/Search';
+import CategoryItem from './src/components/CategoryItem';
 
 export default function App(){
   return (
     <View style={styles.container}>
     <Header/>
     <Search />
+    <CategoryItem />
     <StatusBar />
     </View>
 
